6ac1a6e6262cb73cfd7084de319703ae604c350b
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-02-28 09:32:54 -0600

    LDMA:
    Functionality to create new TODO's added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-console.log("First test! LDMA")
+const mainForm = document.getElementById("main-form");
+const inputField = document.getElementById("todo-input");
+const submitButton = document.getElementById("submit-button");
+const todoList = document.getElementById("todo-list");
+
+
+const handleSubmitTodo = (e) => {
+    e.preventDefault();
+    
+    const newTodoText = inputField.value;
+    const newListItem = document.createElement("li");
+    newListItem.append(newTodoText);
+
+    todoList.append(newListItem);
+
+    inputField.value = "";
+}
+
+mainForm.addEventListener("submit", handleSubmitTodo);
+
