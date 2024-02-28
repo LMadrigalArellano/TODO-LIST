3b7deac5c888440a1994e780d9156015315dc841
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-02-28 10:27:33 -0600

    LDMA:
    TODO list now accepts 8 items as maximum
    A message is displayed if the user tries to add more than 8 items

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,14 @@ const mainForm = document.getElementById("main-form");
 const inputField = document.getElementById("todo-input");
 const submitButton = document.getElementById("submit-button");
 const todoList = document.getElementById("todo-list");
+const MAXTODOITEMS = 8;
+
+
+const listCanReceiveMoreItems = (list, maxItems) => {
+    const currentItemCount = list.getElementsByTagName("li").length;
+
+    return  currentItemCount < maxItems;
+}
 
 const validateInput = (text) => {
     let result = "";
@@ -17,28 +25,34 @@ const validateInput = (text) => {
 
 const handleSubmitTodo = (e) => {
     e.preventDefault();
+
+    if(listCanReceiveMoreItems(todoList, MAXTODOITEMS)){
+        const newTodoText = inputField.value;
+        const isTodoTextValid = validateInput(newTodoText);
     
-    const newTodoText = inputField.value;
-    const isTodoTextValid = validateInput(newTodoText);
-
-    if(isTodoTextValid.length > 0){
-        alert(isTodoTextValid);
-    } else{
-        const newListItem = document.createElement("li");
-        const newListItemButton = document.createElement("button");
-
-        newListItem.classList.add("todo-list-item");
-        newListItemButton.classList.add("todo-list-item-button");
-
-        newListItem.append(newTodoText);
-        newListItemButton.append("X");
-
-        newListItem.appendChild(newListItemButton);
-
-        todoList.append(newListItem);
+        if(isTodoTextValid.length > 0){
+            alert(isTodoTextValid);
+        } else{
+            const newListItem = document.createElement("li");
+            const newListItemButton = document.createElement("button");
     
-        inputField.value = "";
+            newListItem.classList.add("todo-list-item");
+            newListItemButton.classList.add("todo-list-item-button");
+    
+            newListItem.append(newTodoText);
+            newListItemButton.append("X");
+    
+            newListItem.appendChild(newListItemButton);
+    
+            todoList.append(newListItem);
+        
+            inputField.value = "";
+        }
+    } else {
+        alert(`A maximum of ${MAXTODOITEMS} items are allowed in the list`)
     }
+    
+   
     
 
 
