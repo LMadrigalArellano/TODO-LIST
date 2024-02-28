1b4b3b59221442d169bffc2b4b43169b2f162db2
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-02-28 09:42:17 -0600

    LDMA:
    Text field now displays a message if its value is empty or less than 3 characters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,17 +3,38 @@ const inputField = document.getElementById("todo-input");
 const submitButton = document.getElementById("submit-button");
 const todoList = document.getElementById("todo-list");
 
+const validateInput = (text) => {
+    let result = "";
+
+    if(text.length === 0){
+        result = "Empty values are not allowed";
+    } else if(text.length < 3){
+        result = "Value must be at least 3 characters";
+    }
+    return result;
+}
+
 
 const handleSubmitTodo = (e) => {
     e.preventDefault();
     
     const newTodoText = inputField.value;
-    const newListItem = document.createElement("li");
-    newListItem.append(newTodoText);
+    const isTodoTextValid = validateInput(newTodoText);
 
-    todoList.append(newListItem);
+    if(isTodoTextValid.length > 0){
+        alert(isTodoTextValid);
+    } else{
+        const newListItem = document.createElement("li");
+        newListItem.append(newTodoText);
+    
+        todoList.append(newListItem);
+    
+        inputField.value = "";
+    }
+    
 
-    inputField.value = "";
+
+    
 }
 
 mainForm.addEventListener("submit", handleSubmitTodo);
